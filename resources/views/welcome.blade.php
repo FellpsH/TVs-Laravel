c66d6a459a527ab23ec03e909c795af8b4379c9c
FellpsH <fellipehenrique42@yahoo.com.br>
2022-02-04 13:59:35 -0300

    Primeira versao do site (1.0)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,22 +15,40 @@
     <style>
         body {
             font-family: 'Nunito', sans-serif;
-            background: #008E97;
+            background: whitesmoke;
+            background-image: linear-gradient(45deg, black, purple);
+            height: 100vh;
+            color: white;
+            text-align: center;
+        }
+
+        .calculadora {
+            position: absolute;
+            background-color: rgba(0, 0, 0, 0.8);
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            border-radius: 15px;
+            padding: 15px;
         }
     </style>
 </head>
 
 <body class="antialiased">
-    <p>O valor total das TVs 32 polegadas é: {{$total1}}</p>
-    <p>O valor total das TVs 40 polegadas é: {{$total2}}</p>
-    <p>O valor total final de todas as TVs é: {{$totalt}}</p>
-    <hr>
-    <p>O valor total dos Player é: {{$totalA}}</p>
-    <p>O valor total dos Players Sugundarios é: {{$totalS}}</p>
-    <p>O valor total final de todos os Players é: {{$totalP}}</p>
-    <hr>
-    <p>Incestimento Inicial: {{$valorI}}</p>
-    <p>O valro do Plano é: {{$totalM}}</p>
+    <div class="fundo">
+        <div class="calculadora">
+            <p>O valor total das TVs 32 polegadas é: ${{$total1}}</p>
+            <p>O valor total das TVs 40 polegadas é: ${{$total2}}</p>
+            <p>O valor total final de todas as TVs é: ${{$totalt}}</p>
+            <hr>
+            <p>O valor total dos Player é: ${{$totalA}}</p>
+            <p>O valor total dos Players Secundário é: ${{$totalS}}</p>
+            <p>O valor total final de todos os Players é: ${{$totalP}}</p>
+            <hr>
+            <p>Incestimento Inicial: ${{$valorI}}</p>
+            <p>O valro do Plano é: ${{$totalM}}</p>
+        </div>
+        <div>
 </body>
 
 </html>
